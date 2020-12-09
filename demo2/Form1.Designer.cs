9f2fabc2a2b2a0f76cd6f5ae60f8f3f76ce8215a
Nguyen Loan <loanloannguyen0038@gmail.com>
2020-12-09 14:18:08 +0700

    loan ad

---
+++ demo2/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace demo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VND2USD = new System.Windows.Forms.Button();
+            this.lblKetQua = new System.Windows.Forms.Label();
+            this.txtMoney = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // VND2USD
+            // 
+            this.VND2USD.Location = new System.Drawing.Point(120, 168);
+            this.VND2USD.Name = "VND2USD";
+            this.VND2USD.Size = new System.Drawing.Size(104, 43);
+            this.VND2USD.TabIndex = 0;
+            this.VND2USD.Text = "VND2USD";
+            this.VND2USD.UseVisualStyleBackColor = true;
+            this.VND2USD.Click += new System.EventHandler(this.VND2USD_Click);
+            // 
+            // lblKetQua
+            // 
+            this.lblKetQua.AutoSize = true;
+            this.lblKetQua.Location = new System.Drawing.Point(271, 179);
+            this.lblKetQua.Name = "lblKetQua";
+            this.lblKetQua.Size = new System.Drawing.Size(51, 20);
+            this.lblKetQua.TabIndex = 1;
+            this.lblKetQua.Text = "label1";
+            // 
+            // txtMoney
+            // 
+            this.txtMoney.Location = new System.Drawing.Point(140, 92);
+            this.txtMoney.Name = "txtMoney";
+            this.txtMoney.Size = new System.Drawing.Size(100, 26);
+            this.txtMoney.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMoney);
+            this.Controls.Add(this.lblKetQua);
+            this.Controls.Add(this.VND2USD);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button VND2USD;
+        private System.Windows.Forms.Label lblKetQua;
+        private System.Windows.Forms.TextBox txtMoney;
+    }
+}
+
---
+++ demo2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demo2
+{
+    public partial class Form1 : Form
+    {
+        private ServiceReference1.WebService1SoapClient ws;
+        public Form1()
+        {
+            InitializeComponent();
+            ws = new ServiceReference1.WebService1SoapClient();
+        }
+
+        private void VND2USD_Click(object sender, EventArgs e)
+        {
+            double mon = Double.Parse(txtMoney.Text);
+
+            double usd = ws.VND2USD(mon);
+
+            lblKetQua.Text = usd.ToString();
+        }
+    }
+}
